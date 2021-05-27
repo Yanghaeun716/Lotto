1475425185932fb1edd1b6dcf10c7904bed5fb0a
H-E Yang <201921071@sangmyung.kr>
2021-05-27 13:28:17 +0900

    Random number generation

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -4,20 +4,61 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.cardview.widget.CardView
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun getRandomLottoNumber (): Int{
+    return Random().nextInt(45)+1
+}
+fun getRandomLottoNumbers(): MutableList<Int>{
+    val lottoNumbers = mutableListOf<Int>()
+
+    while(true){
+        val number = getRandomLottoNumber()
+        var flag_exist = 0
+
+        if(lottoNumbers.size <1){
+            lottoNumbers.add(number)
+            continue
+        }
+        else {
+            for (j in 0 until lottoNumbers.size) {
+                if (number == lottoNumbers[j]) {
+                    flag_exist = 1
+                    break
+                }
+            }
+            if (flag_exist == 0)
+                lottoNumbers.add(number)
+            if (lottoNumbers.size >= 6)
+                break
+        }
+    }
+    return lottoNumbers
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val cardConstellation = findViewById<CardView>(R.id.cardConstellation)
-        val cardName = findViewById<CardView>(R.id.cardName)
 
+        val cardRandom = findViewById<CardView>(R.id.cardRandom)
+        cardRandom.setOnClickListener{
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+            startActivity(intent)
+        }
+
+        val cardConstellation = findViewById<CardView>(R.id.cardConstellation)
         cardConstellation.setOnClickListener{
-            startActivity(Intent(this@MainActivity, ConstellationActivity::class.java))
+            startActivity(Intent(this, ConstellationActivity::class.java))
         }
+
+        val cardName = findViewById<CardView>(R.id.cardName)
         cardName.setOnClickListener{
-            startActivity(Intent(this@MainActivity, NameActivity::class.java))
+            startActivity(Intent(this, NameActivity::class.java))
         }
+
     }
 }
